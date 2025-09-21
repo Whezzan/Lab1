b43ea0b871af6a236fc305a277880371adbf4365
Whezzan <d.skonneberg@gmail.com>
2025-09-21 11:30:40 +0200

    Fixade så att hela original-strängen skrivs ut på varje rad.

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,60 +6,55 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Mata in valfri sträng av bokstäver och siffror");
+            Console.WriteLine("Mata in valfri sträng av bokstäver och siffror:");
             string input = Console.ReadLine(); 
             Console.WriteLine();
 
-            
             long sum = 0;
-
 
             for (int i = 0; i < input.Length; i++)
             {
-                
-                if (char.IsNumber(input[i]) == false)
-                {
+                if (!char.IsNumber(input[i]))
                     continue;
-                }
 
                 for (int j = i + 1; j < input.Length; j++)
                 {
+                    if (!char.IsNumber(input[j]))
+                        break;
                     
-                    if (char.IsNumber(input[j]) == true)
-                    {
-                        if (input[i] == input[j])
+                    if (input[i] != input[j])
                         {
-                            
-                            string numberMatch = input.Substring(i, j - i + 1);
-                            string firstMatch = input.Substring(0, input.IndexOf(numberMatch));
-                            string secondMatch = input.Substring(i + 1);
-
-                            
-                            Console.Write(firstMatch);
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.Write(numberMatch);
-                            Console.ResetColor();
-                            Console.WriteLine(secondMatch);
-
-                            
-                            long result;
-                            long.TryParse(numberMatch, out result);
-                            sum += result;
-                            break;
-                        }
+                        continue;
                     }
-                    else
-                    {
+                        // Hämta matchande delsträng
+                            string numberMatch = input.Substring(i, j - i + 1);
+
+                        // Skriv hela strängen tecken för tecken
+                        for (int k = 0; k < input.Length; k++)
+                        {
+                            if (k >= i && k <= j)
+                            {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                                Console.Write(input[k]);
+                            Console.ResetColor();
+                            }
+                            else
+                            {
+                                Console.Write(input[k]);
+                            }
+                        }
+                        Console.WriteLine();
+
+                        // Lägg till summan
+                        if (long.TryParse(numberMatch, out long result))
+                        {
+                            sum += result;
+                    }
                         break;
                     }
                 }
-            }
-            
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine($"Summan av delsträngarna är {sum}");
-
-
             Console.ReadKey();
         }
     }
